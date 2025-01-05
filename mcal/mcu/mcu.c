6781f1020f6ad4fca962742682869ce94102f23c
ksalwin <kasjan.salwin@gmail.com>
2025-01-05 18:35:58 +0100

    CHANGE HCLK to 32 MHz

--- mcal/mcu/mcu.c
+++ mcal/mcu/mcu.c
@@ -6,6 +6,7 @@ void mcu_clock_init(void) {
 	/***** Clock source *****/
 	// Enable HSI as clock source
 	SET_BIT(RCC->CR, RCC_CR_HSION);
+	// HSI = 8 MHz
 
 	// Wait for stable HSI
 	while(READ_BIT(RCC->CR, RCC_CR_HSIRDY) == 0);
@@ -15,7 +16,8 @@ void mcu_clock_init(void) {
 	CLEAR_BIT(RCC->CFGR, RCC_CFGR_PLLSRC);
 
 	// PLL multiplication factor
-	SET_BIT(RCC->CFGR, RCC_CFGR_PLLMULL4);
+	SET_BIT(RCC->CFGR, RCC_CFGR_PLLMULL8);
+	// SYSCLK is HSI(4) * PLL(8) = 32 MHz
 
 	// System clock switch: PLL 
 	SET_BIT(RCC->CFGR, RCC_CFGR_SW_PLL);
@@ -25,6 +27,11 @@ void mcu_clock_init(void) {
 
 	// Wait for PLL ready
 	while(READ_BIT(RCC->CR, RCC_CR_PLLRDY) == 0);
+
+	/***** HCLK/AHB prescaler  *****/
+	// Prescaler 1
+	CLEAR_BIT(RCC->CFGR, RCC_CFGR_HPRE);
+	// HCLK = 32 MHz
 }
 
 void mcu_systick_init(void) {

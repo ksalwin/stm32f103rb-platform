8213de20ef102ad55797a7c5ce8059ec91b39005
ksalwin <kasjan.salwin@gmail.com>
2025-01-09 22:55:22 +0100

    CHANGE make gpio init generic

--- mcal/gpio/gpio.c
+++ mcal/gpio/gpio.c
@@ -6,33 +6,30 @@
 
 void gpio_init(void) {
 	uint8_t i;
-	//uint8_t pin;
 
 	// Initialize all pins according to the gpio_cfg
 	for(i = 0; i < gpio_cfg_count; i++)
 	{
-		//pin = gpio_cfg[i].pin;
+		uint8_t 		pin_offset;
+		__IO uint32_t	*cfg_reg;
 
-		if(gpio_cfg[i].pin < GPIO_PINS_IN_CRL_REG)
-		{
-			// Clear mode and cnf of a pin
-			gpio_cfg[i].GPIOx->CRL &= ~(0x0000000f << (gpio_cfg[i].pin * 4));
-
-			// Set cnf 
-			SET_BIT(
-				gpio_cfg[i].GPIOx->CRL,
-				(uint32_t) gpio_cfg[i].cnf << (gpio_cfg[i].pin * 4)
-			);
-
-			// Set mode
-			SET_BIT(
-				gpio_cfg[i].GPIOx->CRL,
-				(uint32_t) gpio_cfg[i].mode << (gpio_cfg[i].pin * 4)
-			);
-
-    		//GPIOA->CRL &= ~(GPIO_CRL_CNF5);
-			//GPIOA->CRL |= GPIO_CRL_MODE5_1;
-		    //GPIOA->CRL &= ~GPIO_CRL_MODE5_0;
+		// Pins 0-7  are configured in CRL
+		// Pins 8-15 are configured in CRH
+		if(gpio_cfg[i].pin < GPIO_PINS_IN_CRL_REG) {
+			cfg_reg		= &gpio_cfg[i].GPIOx->CRL;
+			pin_offset	= gpio_cfg[i].pin * 4;
+		} else {
+			cfg_reg		= &gpio_cfg[i].GPIOx->CRH;
+			pin_offset	= (gpio_cfg[i].pin - 8) * 4;
 		}
+			
+		// Clear mode and cnf of a pin
+		CLEAR_BIT(*cfg_reg, 0x0000000f << pin_offset);
+
+		// Set cnf 
+		SET_BIT(*cfg_reg, (uint32_t) gpio_cfg[i].cnf  << pin_offset);
+
+		// Set mode
+		SET_BIT(*cfg_reg, (uint32_t) gpio_cfg[i].mode << pin_offset);
 	}
 }
